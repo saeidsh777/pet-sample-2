501947a9f54465ca93557b6fe5d5cd4dcc059952
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2023-12-23 22:09:15 +0330

    create card box

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../../Components/Header/Header";
 import { Github } from "react-bootstrap-icons";
 import "./Home.css";
 import BtnBone from "../../Components/BtnBone/BtnBone";
+import OurFeature from "../../Components/OurFeature/OurFeature";
 export default function Home() {
   return (
     <>
@@ -12,11 +13,11 @@ export default function Home() {
             <div className="col-12 col-lg-5 text-center text-lg-start mt-5 mt-lg-0 ps-c">
               <h1>Pets Boarding Services</h1>
 
-              <div className="d-flex align-items-center my-lg-4">
-                <div className="pe-0 pe-lg-3 header-icon-parent">
+              <div className="d-flex align-items-center my-4 justify-content-center">
+                <div className="px-3 header-icon-parent">
                   <Github className="header-icon" />
                 </div>
-                <div className="ps-0 ps-lg-3">
+                <div className="px-3">
                   <h3>Boarding Any Pets</h3>
                   <p className="mb-0 gray-t">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -24,8 +25,9 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-
-              <BtnBone/>
+              <div className="d-flex justify-content-center justify-content-lg-start">
+                <BtnBone />
+              </div>
             </div>
 
             <div className="col-12 col-lg-7">
@@ -38,6 +40,8 @@ export default function Home() {
           </div>
         </div>
       </Header>
+
+      <OurFeature />
     </>
   );
 }
