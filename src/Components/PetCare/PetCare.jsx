8ad2707937e82f92935ae867b5b3a7c12a3ae0eb
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2023-12-24 16:14:18 +0330

    edit component PetCare

--- src/Components/PetCare/PetCare.jsx
+++ src/Components/PetCare/PetCare.jsx
@@ -45,6 +45,74 @@ export default function PetCare() {
                   <span className="orng-t">&#x2715;</span>
                 </li>
               </ul>
+              <div className="mt-5 care-price-box">
+                <span className="align-top fw-bold fs-2">$</span>
+                <span className="care-price">50</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-lg-3">
+            <div className="pet-care-card orng-b">
+              <h5 className="white-t mb-3">PET CARE</h5>
+              <h2 className="white-t mb-3">10 Day</h2>
+              <h2>
+                <House />
+              </h2>
+              <ul className="m-0 p-0 mt-4">
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0 white-t">Double room</p>
+                  <span className="white-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0 white-t">Socialise</p>
+                  <span className="white-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0 white-t">Brush</p>
+                  <span className="white-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0 white-t">Pet TV</p>
+                  <span className="white-t">&#x2715;</span>
+                </li>
+              </ul>
+              <div className="mt-5 care-price-box white-t">
+                <span className="align-top fw-bold fs-2">$</span>
+                <span className="care-price">50</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-lg-3">
+            <div className="pet-care-card">
+              <h5 className="orng-t mb-3">PET CARE</h5>
+              <h2 className="mb-3">20 Day</h2>
+              <h2>
+                <House />
+              </h2>
+              <ul className="m-0 p-0 mt-4">
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0">Double room</p>
+                  <span className="green-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0">Socialise</p>
+                  <span className="green-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0">Brush</p>
+                  <span className="green-t">&#10003;</span>
+                </li>
+                <li className="d-flex justify-content-between align-items-center mb-2">
+                  <p className="m-0">Pet TV</p>
+                  <span className="orng-t">&#x2715;</span>
+                </li>
+              </ul>
+              <div className="mt-5 care-price-box">
+                <span className="align-top fw-bold fs-2">$</span>
+                <span className="care-price">50</span>
+              </div>
             </div>
           </div>
         </div>
